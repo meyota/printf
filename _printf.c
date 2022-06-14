ed75703e0654a29f758f0a385e484dcee58fa379
meyota <meyota08@gmail.com>
2022-06-14 02:07:12 -0700

    print

--- _printf.c
+++ _printf.c
@@ -8,42 +8,38 @@
  */
 int _printf(const char *format, ...)
 {
-	int sum = 0;
+	int (*pfunc)(va_list, flags_t *);
+	const char *p;
 	va_list ap;
-	char *p, *start;
-	params_t params = PARAMS_INIT;
+	flags_t flags = {0, 0, 0};
+	int sum = 0;
 
 	va_start(ap, format);
-
 	if (!format || (format[0] == '%' && !format[1]))
 		return (-1);
 	if (format[0] == '%' && format[1] == ' ' && !format[2])
 		return (-1);
-	for (p = (char *)format; *p; p++)
+	for (p = format; *p; p++)
 	{
-		init_params(&params, ap);
-		if (*p != '%')
-		{
-			sum += _putchar(*p);
-			continue;
-		}
-		start = p;
-		p++;
-		while (get_flag(p, &params))
+		if (*p == '%')
 		{
 			p++;
+			if (*p == '%')
+			{
+				sum += _putchar('%');
+				continue;
+			}
+			while (get_flag(*p, &flags))
+				p++;
+			pfunc = get_print(*p);
+			sum += (pfunc)
+				? pfunc(ap, &flags)
+				:_printf("%%%c", *p);
 		}
-		p = get_width(p, &params, ap);
-		p = get_precision(p, &params, ap);
-		if (get_modifier(p, &params))
-			p++;
-		if (!get_specifier(p))
-			sum += print_from_to(start, p,
-				params.l_modifier || params.h_modifier ? p - 1 : 0);
 		else
-			sum += get_print_func(p, ap, &params);
+			sum += _putchar(*p);
 	}
-	_putchar(BUF_FLUSH);
+	_putchar(-1);
 	va_end(ap);
 	return (sum);
 }
