e1ada8b61c6a36d1f10a10cb836cbd8720725777
meyota <meyota08@gmail.com>
2022-07-09 10:19:51 -0700

    project

--- _printf.c
+++ _printf.c
@@ -1,104 +1,48 @@
 #include "main.h"
+
 /**
- * print_character - prints character
- * @arg: va_list parameter
- * Description: print character
- * Return: 1
+ * _printf - produces output according to a format
+ * @format: format string containing the characters and the specifiers
+ * Description: this function will call the get_print() function that will
+ * determine which printing function to call depending on the conversion
+ * specifiers contained into fmt
+ * Return: length of the formatted output string
  */
-int print_character(va_list arg)
+int _printf(const char *format, ...)
 {
-	int i;
+    int (*pfunc)(va_list, flags_t *);
+    const char *p;
+    va_list arguments;
+    flags_t flags = {0, 0, 0};
 
-	i = va_arg(arg, int);
-	_putchar(i);
+    register int count = 0;
 
-	return (1);
-}
-/**
- * print_sign - print sign
- * @arg: va_list parameter
- * @base: base 10, 8, 16, 2 etc..
- * Description: print numbers and signed
- * Return: num of characters
- */
-int print_sign(va_list arg, int base)
-{
-	int i = 0, cont = 0;
-	char *s;
-
-	i = va_arg(arg, int);
-	if (i < 0)
-	{
-		i = -(i);
-		_putchar('-');
-		cont += 1;
-	}
-	s = convert_to("0123456789ABCDEF", i, base);
-	_puts(s);
-	cont += _strlen(s);
-	return (cont);
-}
-/**
- * print_unsign - print_unsign
- * @arg: va_list parameter
- * @base: base 10, 8, 16 etc..
- * Description: print numbers without signed
- * Return: num of characters
- */
-int print_unsign(va_list arg, int base)
-{
-	int cont = 0;
-	unsigned int i;
-	char *s;
-
-	i = va_arg(arg, unsigned int);
-	s = convert_to("0123456789ABCDEF", i, base);
-	_puts(s);
-	cont = _strlen(s);
-
-	return (cont);
-
-}
-/**
- * print_string - print string
- * @arg: va_list parameter
- * Description: print string
- * Return: num of characters
- */
-int print_string(va_list arg)
-{
-	char *s;
-	int cont = 0;
-
-	s = va_arg(arg, char *);
-	if (!s)
-	{
-		s = "(null)";
-		_puts(s);
-
-		return (_strlen(s));
-	}
-	_puts(s);
-	cont = _strlen(s);
-	return (cont);
-}
-/**
- * print_base16_upper_lower - print_base16_upper_lower
- * @arg: va_list parameter
- *@representation: pointer parameter
- * Description: This function takes 0123456789ABCDEF or 0123456789abcdef
- * in representation parameter for print hexadecimal format
- * Return: num of characters
- */
-int print_base16_upper_lower(va_list arg, char *representation)
-{
-	unsigned int i = 0, cont = 0;
-	char *s;
-
-	i = va_arg(arg, unsigned int);
-	s = convert_to(representation, i, 16);
-	_puts(s);
-	cont = _strlen(s);
-	return (cont);
-
+    va_start(arguments, format);
+    if (!format || (format[0] == '%' && !format[1]))
+        return (-1);
+    if (format[0] == '%' && format[1] == ' ' && !format[2])
+        return (-1);
+    for (p = format; *p; p++)
+    {
+        if (*p == '%')
+        {
+            p++;
+            if (*p == '%')
+            {
+                count += _putchar('%');
+                continue;
+            }
+            while (get_flag(*p, &flags))
+                p++;
+            pfunc = get_print(*p);
+            count += (pfunc)
+                         ? pfunc(arguments, &flags)
+                         : _printf("%%%c", *p);
+        }
+        else
+            count += _putchar(*p);
+    }
+    _putchar(-1);
+    va_end(arguments);
+    return (count);
 }
