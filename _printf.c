fe4822221ea2116125b8894e8f1d3b9b8f88a2c5
meyota <meyota08@gmail.com>
2022-09-24 08:38:14 -0700

    print

--- _printf.c
+++ _printf.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * check_for_specifiers - checks if there is a valid format specifier
+ * check_specifier - checks if there is a valid format specifier
  * @format: possible format specifier
  *
  * Return: pointer to valid function or NULL
  */
 
-static int (*check_for_specifiers(const char *format))(va_list)
+static int (*check_specifier(const char *format))(va_list)
 {
 	unsigned int i;
 	print_type p[] = {
