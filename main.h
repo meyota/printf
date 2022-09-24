026cf8332a7cec1b6cfc69e9a0410562b65d3fb2
meyota <meyota08@gmail.com>
2022-09-24 05:35:09 -0700

    print

--- main.h
+++ main.h
@@ -25,5 +25,6 @@ int print_c(va_list c);
 int print_s(va_list s);
 int print_d(va_list d);
 int print_i(va_list i);
+int print_b(va_list b);
 int (*check_specifier(const char *format))(va_list);
 #endif
