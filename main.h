b8b7596381b9f97f0d8b6e17b0748e578b8832e1
meyota <meyota08@gmail.com>
2022-09-24 05:17:28 -0700

    print

--- main.h
+++ main.h
@@ -23,4 +23,7 @@ int _printf(const char *format, ...);
 int _putchar(char c);
 int print_c(va_list c);
 int print_s(va_list s);
+int print_d(va_list d);
+int print_i(va_list i);
+int (*check_specifier(const char *format))(va_list);
 #endif
