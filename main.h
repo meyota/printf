c89b32445eb2eb8752906340d7bfdff4b599c787
meyota <meyota08@gmail.com>
2022-09-24 04:35:22 -0700

    print

---
+++ main.h
@@ -0,0 +1,25 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+ * struct print_struct - structure for printing various types
+ * @t: type to print
+ * @f: function to print
+ */
+
+typedef struct print_struct
+{
+	char *t;
+	int (*f)(va_list);
+} print_type;
+
+
+int _printf(const char *format, ...);
+int _putchar(char c);
+
+#endif
